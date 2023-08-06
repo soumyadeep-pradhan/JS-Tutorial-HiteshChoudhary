f27f809a4c9db4e874dd50efb302613ea473f604
soumyadeep-pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2023-08-06 06:43:44 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accoundId = 123
+let accountEmail  = "abc@google.com"
+var accountPassword = 1123
+acciuntCity = "kolkata"
+let accountstate;
+// accoundId = 456
+/*Dont use var only use let due to block scope and functional scope issue */
+accountEmail = "nbm@gmail.com"
+accountPassword = 465
+acciuntCity = "delhi"
+console.log(accoundId);
+console.table([accoundId,accountEmail,accountPassword,acciuntCity,accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+// alert(3*3) // we are using nodejs, not browser
+console.log(3+
+    3); //code readability should be high
+console.log("soumya");
+let name = "soumya"
+let age = 18
+let isLoggedIn =false
+let state = null
+/*
+number=>2 to power 53
+bigint
+string
+boolean
+null=> standalone value
+undefined 
+symbol =>uniqueness
+*/
+/* object*/
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined type
