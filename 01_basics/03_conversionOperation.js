62ae15e996e792a2deb855264d7c053fac644aa5
soumyadeep-pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2023-08-06 14:40:41 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+//let score = "3n4"//not a number NaN but its type is number
+// let score = null //0 
+let score =undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+/* "33"->33
+"33av"->NaN
+true->1 false->0
+*/
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+/* 
+1=>true 0=>false
+""=>false
+"sac"=>true
+*/
