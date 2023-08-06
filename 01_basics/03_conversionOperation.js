6d056ab5bf4fede002d9aa9544965cdf458de194
soumyadeep-pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2023-08-06 15:36:45 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,11 +17,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 /* 
 1=>true 0=>false
 ""=>false
 "sac"=>true
 */
+/*--------Operation------ */
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Soumyadeep"
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("2"+1+2);
+// console.log(1+2+"2");
+
+console.log(+true);//1
+//console.log(true+);//
+// console.log(+"");
+let num1,num2,num3;
+num1=num2=num3=2+2
+console.log(num3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log("2">1);
+// console.log("02">1);
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+/*equality check == and comparison >< >= <= work differently
+comparison convert null to number, treating it as 0 */
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+console.log("2"==2);//conversion takes place
+console.log("2"===2);//check data type also
