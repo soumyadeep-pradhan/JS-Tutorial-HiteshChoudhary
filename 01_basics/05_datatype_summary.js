5b22d0b29c04ba4931bcb9b225632b6549ac5d65
soumyadeep-pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2023-08-07 18:29:21 +0000

    data tyoe summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,33 @@
+/*Primitive & NOn-Primitive*/
+//Primitive:call by value 7 types=> String, Number, Boolean, null, undefined, symbol, BigInt
+//Reference (Non Primitive) : Array, Objectsm, Functions
+//JS dynamically types
+const score = 40
+const scoreValue =100.3 //decimal has no special , no float,int. any number is number
+const isLoggedIn =false
+const outsideTemp = null // it is empty, it is not 0
+let userEmail;//undefined
+const id =Symbol('123')
+const anotherID = Symbol('123')//false
+// console.log(id === anotherID);
+const bigNumber = 1864624664641n
+// console.log(typeof bigNumber);
+const heros = ["shaktiman","naaraj", "doga"]//arary
+
+let myObj ={
+    name:"Soumyadeep",
+    age:21,
+}//object
+
+//funtion can be stored in variable
+const myfunction = function(){
+    console.log("hello World");
+}
+// console.log(typeof outsideTemp);//object
+// console.log(typeof userEmail);//object
+console.log(typeof myfunction);//function or obejctfuntion 
+/*
+type of val         Result
+undefined           undefined
+null                object
+ */
