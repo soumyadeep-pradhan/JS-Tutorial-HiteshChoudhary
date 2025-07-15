0d9c9c8fffbaeded00672c881d0edf9f4e591790
Soumyadeep Pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2025-07-15 01:32:20 +0000

    add

--- 06_DOM/browser.js
+++ 06_DOM/browser.js
@@ -1,5 +1,10 @@
 console.log(window)
 console.log(window.document)
 console.log(document) //no need to write window.document always, we get html
-console.dir(document) //also get remaining information
+console.dir(document) //also get remaining information, if required
 
+console.log(document.baseURI);//base url of webpage
+
+console.log(document.links);//html collection of links not array!!
+
+console.log(document.links[]2);
