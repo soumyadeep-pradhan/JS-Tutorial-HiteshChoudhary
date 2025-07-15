073a85b31685cc4436d62f8d6d127b70ded39d8d
Soumyadeep Pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2025-07-15 01:20:34 +0000

    notes

---
+++ 06_DOM/browser.js
@@ -0,0 +1,5 @@
+console.log(window)
+console.log(window.document)
+console.log(document) //no need to write window.document always, we get html
+console.dir(document) //also get remaining information
+
