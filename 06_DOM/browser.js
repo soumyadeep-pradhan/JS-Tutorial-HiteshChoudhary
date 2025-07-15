f593216ddbdea6de93a2970f36a86a502630e1bf
Soumyadeep Pradhan <133474598+soumyadeep-pradhan@users.noreply.github.com>
2025-07-15 01:36:28 +0000

    change

--- 06_DOM/browser.js
+++ 06_DOM/browser.js
@@ -3,8 +3,11 @@ console.log(window.document)
 console.log(document) //no need to write window.document always, we get html
 console.dir(document) //also get remaining information, if required
 
+// https://en.wikipedia.org/wiki/Brendan_Eich
 console.log(document.baseURI);//base url of webpage
 
 console.log(document.links);//html collection of links not array!!
 
-console.log(document.links[]2);
+console.log(document.links[2]);//2nd link
+
+document.getElementById('firstHeading')
